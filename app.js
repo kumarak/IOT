4257a125f3befef4169c6862ab3120bd6f4ee782
kumarak <iit.akshay@gmail.com>
2014-08-28 21:00:17 -0700

    latest changes;

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
-/**
- * Module dependencies.
- */
+var express = require('express');
+var http = require('http');
+var fs = require('fs');
+var routes = require('./routes');
+var sio = require('socket.io');
+var gpio = require('pi-gpio');
+var crypto = require('crypto');
+var async = require('async');
+var wav = require('wav');
+var BinaryServer = require('binaryjs').BinaryServer;
+var server = BinaryServer({port: 9000});
+var lame = require('lame');
 
-var express = require('express'),
-    routes = require('./routes'),
-    sio = require('socket.io'),
-    gpio = require('pi-gpio'),
-    crypto = require('crypto'),
-    async = require('async'),
-    tank = {},
-    _leftMotorFront  = 18,
-    _leftMotorBack   = 12,
-    _rightMotorFront = 15,
-    _rightMotorBack  = 16,
-    app = module.exports = express.createServer(),
-    io = sio.listen(app);
+var servo = {};
+var app = module.exports = express.createServer();
+var io = sio.listen(app);
+var Speaker = require('speaker');
+var mic = require('microphone');
+//var Analyzer = require('web-audio-analyser')(mic);
+var sys = require('sys');
+var exec = require('child_process').exec;
 
-var sys = require('sys'),
-    exec = require('child_process').exec;
+var audioOptions = {channels: 2, bitDepth: 16, sampleRate: 44100};
 
+var speaker = new Speaker({
+  channels: 2,          // 1 channel
+  bitDepth: 16,         // 16-bit samples
+  sampleRate: 44100,    // 48,000 Hz sample rate
+  signed:true
+});
+
+// express framework
 // Configuration
 app.configure(function() {
   app.set('views', __dirname + '/views');
@@ -33,73 +44,148 @@ app.configure('development', function() {
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
-app.configure('production', function() {
-  app.use(express.errorHandler());
-});
-
 // Routes
 app.get('/', routes.index);
 
 app.listen(3000);
-console.log('Listening %d in %s mode', app.address().port, app.settings.env);
+//console.log('Listening %d in %s mode', app.address().port, app.settings.env);
 
-tank.initPins = function(){
-};
+var decoder = lame.Decoder();
+var songs = ['/home/pi/akshayk-iot/intel.mp3'];
+
+// Recursive function that plays song with index 'i'.
+function playSong(i) {
+  //var speaker1    = new Speaker(audioOptions);
+  //var inputStream = fs.createReadStream('/home/pi/akshayk-iot/intel.mp3');
+  // Pipe the read data into the decoder and then out to the speakers
+  //inputStream.pipe(decoder).pipe(speaker1);
+  //speaker.on('flush', function(){
+  //});
+}
+
+
+servo.initPins = function(){ };
 
 function puts(error, stdout, stderr) { sys.puts(stdout) };
 
-tank.moveForward = function(){
-    exec("sudo /home/pi/LED/IOT/servo", puts);
+servo.food = function(socket){
+    exec("sudo /home/pi/iotproject/servo", puts);
     console.log('setting servo pin\n');
 };
 
-tank.moveBackward = function(){
-  async.parallel([
-    gpio.write(_leftMotorBack, 1),
-    gpio.write(_rightMotorBack, 1)
-  ]);
+servo.video = function(socket) {
+    exec("sudo killmotion", puts);
+    exec("/home/pi/akshayk-iot/start.sh", puts);
+    app.get('/', routes.index);
+    console.log('Start video\n');
+    socket.emit('refresh', 0);
+}
+
+servo.audio = function(socket) {
+     playSong(0);
+     console.log('Start audio\n');
+     socket.emit('refresh', 0);
+}
+
+var audioOptions = {channels: 2, bitDepth: 16, sampleRate: 44100};
+
+var songs = [ ];
+
+function done() {
+  console.log("AUdio finished")
+}
+
+servo.call_ = function() {
+    mic.startCapture({'mp3output' : false});
+    var audio_analyzer = new Analyzer(mic.audioStream);
+    mic.audioStream.on('data', function(data) {
+        //process.stdout.write(data);
+    });
+    console.log('Call\n')
+}
+
+servo.stopAll = function(){
+
 };
 
-tank.turnLeft = function(){
-  gpio.write(_rightMotorFront, 1);
-};
-
-tank.turnRight = function(){
-  gpio.write(_leftMotorFront, 1);
-};
-
-tank.stopAllMotors = function(){
-  async.parallel([
-    gpio.write(_leftMotorFront, 0),
-    gpio.write(_leftMotorBack, 0),
-    gpio.write(_rightMotorFront, 0),
-    gpio.write(_rightMotorBack, 0)
-  ]);
-};
-
+// receive io sockets 
 io.sockets.on('connection', function(socket) {
-  
+  sequence = 0;
   socket.on('keydown', function(dir) {
     switch(dir){
-     case 'up':
-        tank.moveForward();
+     case 'food':
+        servo.food(socket);
         break;
-      case 'down':
-        tank.moveBackward();
+      case 'video':
+        servo.video(socket);
         break;
-      case 'left':
-        tank.turnLeft();
+      case 'audio':
+        servo.audio(socket);
         break;
-      case 'right':
-        tank.turnRight();
+      case 'call':
+        servo.call_()
         break;
     }
   });
 
-  socket.on('keyup', function(dir){
-    tank.stopAllMotors();
+  //socket.emit('refresh', sequence++);
+
+  socket.on('disconnect', function() {
+     console.log('disconneted\n');
   });
+
+  socket.on('keyup', function(dir){
+    servo.stopAll();
+  });
+
+  socket.on('stream', function(stream, meta){
+        stream.on('data', function(data){
+            //speaker.write(leftchannel);
+        });
+    });
 
 });
 
-tank.initPins();
+server.on('connection', function(client){
+  console.log('binaryjs connection\n');
+  var fileWriter = null;
+
+  client.on('stream', function(stream, meta) {
+    var fileWriter = new wav.FileWriter('demo.wav', {
+      channels: 2,
+      sampleRate: 44100,
+      bitDepth: 16
+    });
+
+    stream.on('data', function(data){
+       console.log('streaming');
+       process.stdout.write(data);
+       stream.pipe(fileWriter);
+       stream.pipe(speaker);
+       //speaker.write(data);
+    });
+    //stream.pipe(fileWriter);
+    stream.on('end', function() {
+       fileWriter.end();
+       speaker.end();
+    });
+  });
+
+  client.on('close', function() {
+    if (fileWriter != null) {
+      fileWriter.end();
+    }
+  });
+  //
+});
+
+// create http servers
+http.createServer(function(req, res){
+    fs.readFile('./www/index.html',function (err, data){
+        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+        res.write(data);
+        res.end();
+    });
+}).listen(8000);
+
+servo.initPins();
--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,68 +1,103 @@
 $(function () {
 
-   var socket = io.connect(),
-    wIsDown = false,
-    aIsDown = false,
-    sIsDown = false,
-    dIsDown = false;
-
-  $(document).keydown(function(e){
-    switch(e.which){
-      case 87:
-        if(wIsDown) return;
-        wIsDown = true;
-        socket.emit('keydown', 'up');
-        $('.up').addClass('active');
-        break;
-      case 65:
-        if(aIsDown) return;
-        aIsDown = true;
-        socket.emit('keydown', 'left');
-        $('.left').addClass('active');
-        break;
-      case 83:
-        if(sIsDown) return;
-        sIsDown = true;
-        socket.emit('keydown', 'down');
-        $('.down').addClass('active');
-        break;
-      case 68:
-        if(dIsDown) return;
-        dIsDown = true;
-        socket.emit('keydown', 'right');
-        $('.right').addClass('active');
-        break;
-    }
+   var socket = io.connect();
+   var hostname = window.location.hostname;
+   var client = BinaryClient('ws://' + hostname + ':9000');
+//   var stream
+   var session = {
+       audio: true,
+       video: false
+   };
+  
+ client.on('open', function() {
+    //alert('client on');
+  // for the sake of this example let's put the stream in the window
+     window.Stream = client.createStream();
   });
 
-  $(document).keyup(function(e){
-    switch(e.which){
-      case 87:
-        if(!wIsDown) return;
-        wIsDown = false;
-        socket.emit('keyup', 'up');
-        $('.up').removeClass('active');
-        break;
-      case 65:
-        if(!aIsDown) return;
-        aIsDown = false;
-        socket.emit('keyup', 'left');
-        $('.left').removeClass('active');
-        break;
-      case 83:
-        if(!sIsDown) return;
-        sIsDown = false;
-        socket.emit('keyup', 'down');
-        $('.down').removeClass('active');
-        break;
-      case 68:
-        if(!dIsDown) return;
-        dIsDown = false;
-        socket.emit('keyup', 'right');
-        $('.right').removeClass('active');
-        break;
+   navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
+
+   var onFail = function() {
+    alert('sssabc')
+   }
+
+  function convertFloat32ToInt16(buffer) {
+    l = buffer.length;
+    buf = new Int16Array(l);
+    while (l--) {
+      buf[l] = Math.min(1, buffer[l])*0x7FFF;
+       //buf[l] = buffer[l]*0xFFFF;
     }
+    return buf.buffer;
+  }
+
+
+   function recorderProcess(e) {
+    var left = e.inputBuffer.getChannelData(0);
+    window.Stream.write(convertFloat32ToInt16(left));
+    alert('sss');
+   }
+
+   var initializeRecorder = function(stream) {
+    alert("sas");
+    var audioContext = window.AudioContext ||
+                      window.webkitAudioContext;
+
+    var context = new audioContext();
+
+    var microphone = context.createMediaStreamSource(stream);
+    var bufferSize = 2048;
+
+     // create a javascript node
+    var recorder = context.createScriptProcessor(bufferSize, 1, 1);
+    // specify the processing function
+    recorder.onaudioprocess = recorderProcess;
+    // connect stream to our recorder
+    microphone.connect(recorder);
+    // connect our recorder to the previous destination
+    recorder.connect(context.destination);
+  }
+
+  var audioClick = false;
+  var videoClick = false;
+  var foodClick = false;
+  var callClick = false;
+  var micPermission = false;
+
+  $("#foodButton").click(function() {
+  	if(foodClick) return;
+  	foodClick = true;
+    socket.emit('keydown', 'food');
   });
 
+  $("#videoButton").click(function() {
+  	if(videoClick) return;
+  	videoClick = true;
+    socket.emit('keydown', 'video');
+  });
+
+  $("#callButton").click(function() {
+  	if(callClick) return;
+  	callClick = true;
+    socket.emit('keydown', 'call');
+  });
+
+  $("#audioButton").click(function() {
+   if(audioClick) return;
+    audioClick = true;
+//    if(!micPermission)
+    navigator.getUserMedia(session, initializeRecorder, onFail);
+
+    micPermission = true;
+    socket.emit('keydown', 'audio');
+  });
+
+  socket.on('refresh', function(){
+    location.reload(true);
+    audioClick = false;
+    videoClick = false;
+    foodClick = false;
+    callClick = false;
+  });
 
 });
